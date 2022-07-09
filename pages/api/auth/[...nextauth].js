eac6c2646ddce5e533e9746b62fa50ada4c88919
JD <Jd@pinc.nz>
2022-07-10 08:07:13 +1200

    add secret to the providers

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,10 +2,11 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
 export default NextAuth({
+    secret: process.env.SECRET,
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        }),
+        })
     ]
 })
